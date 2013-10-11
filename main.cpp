3f71365e429e3e2815a6ed3b252818f6144fbc15
Yayan <brayan.inf@gmail.com>
2013-10-11 22:32:41 +0200

    Principal
    Paso adelante en la lectura de los cuerpos. Dentro del método ya se
    leen correctamente pero siguen sin salir. En el main se recibe ya
    basura. Buscar manera de que el método nos pueda devolver los cuerpos
    generados o habrá que buscar otra estrategia.

--- main.cpp
+++ main.cpp
@@ -11,22 +11,21 @@ Cuerpo** leerCuerpos(int &cantidad, const string nombreFichero){
 	fEntrada.open(nombreFichero.c_str(), ios::in);
 
 	if(fEntrada.is_open()){
-		int cantCuerpos=0;
-
 		getline(fEntrada, linea); //En la primera línea tenemos la cantidad de cuerpos.
 
-		cantCuerpos=atoi(&linea.c_str()[0]);
-		Cuerpo *cuerpos[cantCuerpos];
+		cantidad=atoi(&linea.c_str()[0]);
+		Cuerpo *cuerpos[cantidad];
 
-		for(int i=0; i<cantCuerpos; i++)
+		for(int i=0; i<cantidad; i++)
 			cuerpos[i]=NULL;
 
 		getline(fEntrada, linea); //Esta debe ser la línea con el cuadrante.
 		getline(fEntrada, linea); //Ahora vienen los cuerpos.
 
+		int aux=0;
 		while(!fEntrada.eof()){
-			int aux=0;
-			float x=0.0, y=0.0, m=0.0;
+			float x=0.0, y=0.0;
+			double m=0.0;
 			string compX="", compY="", masa="";
 			bool primerWS=false, segundoWS=false;
 
@@ -50,16 +49,12 @@ Cuerpo** leerCuerpos(int &cantidad, const string nombreFichero){
 			x=atof(compX.c_str());
 			y=atof(compY.c_str());
 			m=atof(masa.c_str());
-			cout << x << "," << y << "," << m << endl;			
 			cuerpos[aux]=new Cuerpo(x, y, m);
 			aux++;
 
 			getline(fEntrada, linea);
 		}
 		fEntrada.close();
-
-		for(int i=0; i<cantidad; i++)
-			cout << cuerpos[i] << endl;
 
 		return cuerpos;
 	}
@@ -79,7 +74,7 @@ int main(){
 	cuerpos=leerCuerpos(cantidad, fichero);
 
 	for(int i=0; i<cantidad; i++)
-		cout << cuerpos[i] << endl;
+		cout << *cuerpos[i] << endl;
 
 	return 0;
 }
